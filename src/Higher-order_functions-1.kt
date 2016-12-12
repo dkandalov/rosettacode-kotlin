b8ca1b5a62555d8ebb5a1c165dd3c532b0eb5e60
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-12-12 22:51:35 +0000

    more examples

---
+++ src/Higher-order_functions-1.kt
@@ -0,0 +1,7 @@
+
+inline fun higherOrderFunction(x: Int, y: Int, function: (Int, Int) -> Int) = function(x, y)
+
+fun main(args: Array<String>) {
+    val result = higherOrderFunction(3, 5) { x, y -> x + y }
+    println(result)
+}
