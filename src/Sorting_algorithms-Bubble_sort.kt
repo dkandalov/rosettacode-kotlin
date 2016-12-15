85a9256b350ff8522ccffa3d25e8bedb9817a041
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-12-15 23:26:56 +0000

    cleanup examples

--- src/Sorting_algorithms-Bubble_sort.kt
+++ src/Sorting_algorithms-Bubble_sort.kt
@@ -1,10 +1,10 @@
 import java.util.Comparator
 
-fun <T> bubbleSort(a : Array<T>, c: Comparator<T>) {
-    var changed : Boolean
+fun <T> bubbleSort(a: Array<T>, c: Comparator<T>) {
+    var changed: Boolean
     do {
         changed = false
-        for (i in 0 .. a.size - 2) {
+        for (i in 0..a.size - 2) {
             if (c.compare(a[i], a[i + 1]) > 0) {
                 val tmp = a[i]
                 a[i] = a[i + 1]
--- src/Sorting_algorithms-Quicksort.kt
+++ src/Sorting_algorithms-Quicksort.kt
@@ -1,13 +1,12 @@
 import java.util.*
 import java.util.Comparator
 
-fun <T> quickSort(a : List<T>, c : Comparator<T>) : ArrayList<T> {
-    return if (a.isEmpty()) ArrayList(a)
-    else {
-        val boxes = Array(3, {ArrayList<T>()})
-        fun normalise(i : Int) = i / Math.max(1, Math.abs(i))
-        a.forEach { boxes[normalise(c.compare(it, a[0])) + 1].add(it) }
-        arrayOf(0, 2).forEach {boxes[it] = quickSort(boxes[it], c)}
-        boxes.flatMapTo(ArrayList<T>()) {it}
-    }
+fun <T> quickSort(a: List<T>, c: Comparator<T>): ArrayList<T> {
+    if (a.isEmpty()) return ArrayList(a)
+
+    val boxes = Array(3, { ArrayList<T>() })
+    fun normalise(i: Int) = i / Math.max(1, Math.abs(i))
+    a.forEach { boxes[normalise(c.compare(it, a[0])) + 1].add(it) }
+    arrayOf(0, 2).forEach { boxes[it] = quickSort(boxes[it], c) }
+    return boxes.flatMapTo(ArrayList<T>()) { it }
 }
