ca38ad011059b251a344d0be1ede44479921d0ba
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-01-07 17:30:34 +0300

    more examples

---
+++ src/Program_name.kt
@@ -0,0 +1,10 @@
+package `program_name`
+
+// version 1.0.6
+
+// 'progname.kt' packaged as 'progname.jar'
+
+fun main(args: Array<String>) {
+    println(System.getProperty("sun.java.command"))  // may not exist on all JVMs
+    println(System.getProperty("java.vm.name")) 
+}
---
+++ src/System_time.kt
@@ -0,0 +1,7 @@
+package `system_time`
+
+// version 1.0.6
+
+fun main(args: Array<String>) {
+    println("%tc".format(System.currentTimeMillis()))
+}
