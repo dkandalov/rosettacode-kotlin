9963444f9344f301b1a9d90a79817d9baea6fda8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-01-09 13:58:00 +0000

    cleanup balanced brackets example

--- src/Balanced_brackets.kt
+++ src/Balanced_brackets.kt
@@ -5,37 +5,32 @@ package `balanced_brackets`
 import java.util.*
 
 fun isBalanced(s: String): Boolean {
-    if (s.length == 0) return true
-    var countLeft: Int = 0  // counts number of left brackets so far unmatched
+    if (s.isEmpty()) return true
+    var countLeft = 0  // number of left brackets so far unmatched
     for (c in s) {
-        if (c == '[')
-            countLeft += 1
-        else if (countLeft > 0)
-            countLeft--
-        else
-            return false
-    } 
+        if (c == '[') countLeft++
+        else if (countLeft > 0) countLeft--
+        else return false
+    }
     return countLeft == 0
 }
 
 fun main(args: Array<String>) {
-    // checking examples in task description
-    val brackets = arrayOf("", "[]", "][", "[][]", "][][", "[[][]]", "[]][[]") 
+    println("Checking examples in task description:")
+    val brackets = arrayOf("", "[]", "][", "[][]", "][][", "[[][]]", "[]][[]")
     for (b in brackets) {
         print(if (b != "") b else "(empty)")
-        println("\t  " + if (isBalanced(b)) "OK" else "NOT OK") 
+        println("\t  " + if (isBalanced(b)) "OK" else "NOT OK")
     }
     println()
-    // checking 7 random strings of brackets of length 8 say
-    val rand = Random()
-    var s: String
-    var r: Int
-    for (i in 1 .. 7) {
-        s = ""
-        for (j in 1 .. 8) {
-            r = rand.nextInt(2)
-            if (r == 0) s += '[' else  s += ']' 
+
+    println("Checking 7 random strings of brackets of length 8:")
+    val r = Random()
+    (1..7).forEach {
+        var s = ""
+        for (j in 1..8) {
+            if (r.nextInt(2) == 0) s += '[' else s += ']'
         }
-        println("$s  " + if (isBalanced(s)) "OK" else "NOT OK") 
-    }   
+        println("$s  " + if (isBalanced(s)) "OK" else "NOT OK")
+    }
 }
