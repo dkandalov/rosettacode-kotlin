cf7208ed28530033e950e36eebd40b875dbd766c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-01-19 23:00:45 +0000

    more examples

---
+++ src/Colour_bars-Display.kt
@@ -0,0 +1,29 @@
+package `colour_bars_display`
+
+// version 1.0.6
+
+import java.awt.Color
+import java.awt.Graphics
+import javax.swing.JFrame
+
+class ColorFrame(width: Int, height: Int): JFrame() {
+    init {
+        defaultCloseOperation = EXIT_ON_CLOSE
+        setSize(width, height)
+        setVisible(true)
+    }
+
+    override fun paint(g: Graphics) {
+        val colors = listOf(Color.black, Color.red,  Color.green,  Color.blue,
+		            Color.pink,  Color.cyan, Color.yellow, Color.white)
+        val size = colors.size
+        for (i in 0 until size) {
+            g.color = colors[i]
+            g.fillRect(width / size * i, 0, width / size, height)
+        }
+    }            
+} 
+
+fun main(args: Array<String>) {
+    ColorFrame(400, 400)
+}
---
+++ src/GUI-Maximum_window_dimensions.kt
@@ -0,0 +1,26 @@
+package `gui_maximum_window_dimensions`
+
+// version 1.0.6
+
+import java.awt.*
+import javax.swing.JFrame
+
+class Test : JFrame() {
+    init {
+        val r = Regex("""\[.*\]""")
+        val toolkit = Toolkit.getDefaultToolkit()
+        val screenSize = toolkit.screenSize
+        println("Physical screen size : ${formatOutput(screenSize, r)}")
+        val insets = toolkit.getScreenInsets(graphicsConfiguration) 
+        println("Insets               : ${formatOutput(insets, r)}") 
+        screenSize.width  -= (insets.left + insets.right);
+        screenSize.height -= (insets.top + insets.bottom);
+        println("Max available        : ${formatOutput(screenSize, r)}")
+    }
+
+    private fun formatOutput(output: Any, r: Regex) = r.find(output.toString())!!.value.replace(",", ", ")
+}
+
+fun main(args: Array<String>) {
+    Test()
+}
