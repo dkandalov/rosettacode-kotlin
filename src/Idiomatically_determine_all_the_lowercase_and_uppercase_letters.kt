d2a90795888e7b5fa06f27efe30a4f5843809382
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-01-27 08:08:26 +0300

    more examples

---
+++ src/Idiomatically_determine_all_the_lowercase_and_uppercase_letters.kt
@@ -0,0 +1,11 @@
+package `idiomatically_determine_all_the_lowercase_and_uppercase_letters`
+
+// version 1.0.6
+
+fun main(args: Array<String>) {
+    print("Lower case : ")
+    for (ch in 'a'..'z') print(ch)
+    print("\nUpper case : ")
+    for (ch in 'A'..'Z') print(ch)
+    println()
+}
---
+++ src/Sort_an_integer_array.kt
@@ -0,0 +1,9 @@
+package `sort_an_integer_array`
+
+// version 1.0.6
+
+fun main(args: Array<String>) {
+   val ints = intArrayOf(6, 2, 7, 8, 3, 1, 10, 5, 4, 9)
+   ints.sort()
+   println(ints.joinToString(prefix = "[", postfix = "]"))
+}
