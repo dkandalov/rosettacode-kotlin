a24ec9eb7186902ac78dde77d6c0e8b7af69bf57
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-02-10 19:03:38 +0000

    more examples

---
+++ src/Host_introspection.kt
@@ -0,0 +1,8 @@
+package `host_introspection`
+
+// version 1.0.6
+
+fun main(args: Array<String>) {
+    println("Word size : ${System.getProperty("sun.arch.data.model")} bits")
+    println("Endianness: ${System.getProperty("sun.cpu.endian")}-endian")
+}
---
+++ src/Program_termination.kt
@@ -0,0 +1,9 @@
+package `program_termination`
+
+// version 1.0.6
+
+fun main(args: Array<String>) {
+    val problem = true
+    if (problem) System.exit(1) // non-zero code passed to OS to indicate a problem
+    println("Program terminating normally")  // this line will not be executed
+}
