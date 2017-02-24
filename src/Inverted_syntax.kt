3426d0bc4da6cb6fbc085ae25f6da6d6ff394a7c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-02-24 13:04:36 +0000

    more examples

---
+++ src/Inverted_syntax.kt
@@ -0,0 +1,11 @@
+package `inverted_syntax`
+
+// version 1.0.6
+
+infix fun Boolean.iif(cond: Boolean) = if (cond) this else !this
+
+fun main(args: Array<String>) {
+    val raining = true
+    val needUmbrella = true iif (raining)
+    println("Do I need an umbrella?  ${if(needUmbrella) "Yes" else "No"}")
+}
