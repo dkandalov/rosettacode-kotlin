bcaf74b3786dcac6cf6071535f222a92fe9f96f1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-03-01 08:27:00 +0000

    more examples

---
+++ src/List_comprehensions.kt
@@ -0,0 +1,16 @@
+package `list_comprehensions`
+
+// version 1.0.6
+
+fun pythagoreanTriples(n: Int) = 
+    (1..n).flatMap { 
+        x -> (x..n).flatMap { 
+            y -> (y..n).filter {
+                z ->  x * x + y * y == z * z 
+            }.map { Triple(x, y, it) } 
+        }
+    }
+
+fun main(args: Array<String>) {
+    println(pythagoreanTriples(20))
+}
---
+++ src/Parse_command-line_arguments.kt
@@ -0,0 +1,5 @@
+package `parse_command_line_arguments`
+
+// version 1.0.6 (packaged as parse_cla.jar)
+
+fun main(args: Array<String>) = println(args.asList())
