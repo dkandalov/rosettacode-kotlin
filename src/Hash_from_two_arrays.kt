036854efc7596600ac36ae71df1ef37887a2b3eb
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-03-07 22:41:30 +0000

    more examples

---
+++ src/Hash_from_two_arrays.kt
@@ -0,0 +1,10 @@
+package `hash_from_two_arrays`
+
+// version 1.1.0
+
+fun main(args: Array<String>) {
+    val names = arrayOf("Jimmy", "Bill", "Barack", "Donald")
+    val ages  = arrayOf(92, 70, 55, 70)
+    val hash  = mapOf(*names.zip(ages).toTypedArray())
+    hash.forEach { println("${it.key.padEnd(6)} aged ${it.value}") }
+}
