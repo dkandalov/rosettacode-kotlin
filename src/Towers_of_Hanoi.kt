299b1f9fd1efd810f467f02b140c31d79369a3b5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-03-14 00:47:59 +0000

    more examples

---
+++ src/Towers_of_Hanoi.kt
@@ -0,0 +1,27 @@
+package `towers_of_hanoi`
+
+// version 1.1.0
+
+class Hanoi(private val disks: Int) {
+    private var moves = 0
+
+    init {
+        println("Towers of Hanoi with $disks disks:\n")
+        move(disks, 'L', 'C', 'R') 
+        println("\nCompleted in $moves moves\n")   
+    }
+
+    private fun move(n: Int, from: Char, to: Char, via: Char) {
+        if (n > 0) {
+            move(n - 1, from, via, to)
+            moves++
+            println("Move disk $n from $from to $to")
+            move(n - 1, via, to, from)
+        }
+    }
+}
+
+fun main(args: Array<String>) {    
+    Hanoi(3)
+    Hanoi(4)
+}
