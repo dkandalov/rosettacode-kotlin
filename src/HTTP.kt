f7508746d00b0bf043b23055a0d2c4bf5927807d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-03-24 22:45:41 +0000

    more examples

---
+++ src/HTTP.kt
@@ -0,0 +1,15 @@
+package `http`
+
+// version 1.1.1
+
+import java.net.URL
+import java.io.InputStreamReader
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val url = URL("http://www.puzzlers.org/pub/wordlists/unixdict.txt")
+    val isr = InputStreamReader(url.openStream())
+    val sc = Scanner(isr)
+    while (sc.hasNextLine()) println(sc.nextLine())
+    sc.close()
+}
