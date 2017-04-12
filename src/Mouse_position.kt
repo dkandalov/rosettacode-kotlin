9d1cddcc8f42f7b845b8c32c9ce556923c63e8ba
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-12 23:13:29 +0100

    more examples

---
+++ src/Mouse_position.kt
@@ -0,0 +1,13 @@
+package `mouse_position`
+
+// version 1.1.1
+
+import java.awt.MouseInfo
+
+fun main(args: Array<String>) {
+    (1..5).forEach {
+        Thread.sleep(1000)
+        val p = MouseInfo.getPointerInfo().location  // gets screen coordinates
+        println("${it}: x = ${"%-4d".format(p.x)} y = ${"%-4d".format(p.y)}")
+    }
+}
