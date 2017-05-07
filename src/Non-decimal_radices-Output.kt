6cf3721927cdc893b79d9e1b4fba89b090b36ecf
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-05-07 14:09:04 +0100

    more examples

---
+++ src/Non-decimal_radices-Output.kt
@@ -0,0 +1,14 @@
+package `non_decimal_radices_output`
+
+// version 1.1.2
+
+fun main(args: Array<String>) {
+    val bases = intArrayOf(2, 8, 10, 16, 19, 36)
+    for (base in bases) print("%6s".format(base))
+    println()
+    println("-".repeat(6 * bases.size))
+    for (i in 0..35) {
+        for (base in bases) print("%6s".format(i.toString(base)))
+        println()
+    }
+}
