7eaf6b621149c55a8d5f95143e1ac5072db58dcf
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-05-10 23:44:33 +0100

    more examples

---
+++ src/URL_decoding.kt
@@ -0,0 +1,10 @@
+package `url_decoding`
+
+// version 1.1.2
+
+import java.net.URLDecoder
+
+fun main(args: Array<String>) {
+    val encoded = arrayOf("http%3A%2F%2Ffoo%20bar%2F", "google.com/search?q=%60Abdu%27l-Bah%C3%A1")
+    for (e in encoded) println(URLDecoder.decode(e, "UTF-8"))
+}
---
+++ src/URL_encoding.kt
@@ -0,0 +1,10 @@
+package `url_encoding`
+
+// version 1.1.2
+
+import java.net.URLEncoder
+
+fun main(args: Array<String>) {
+    val url = "http://foo bar/"
+    println(URLEncoder.encode(url, "utf-8")) // note: encodes space to + not %20
+}
