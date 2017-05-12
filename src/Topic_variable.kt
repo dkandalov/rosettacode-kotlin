8ad1c4fdaaa5a70a3c376c7f04628475d9515f13
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-05-12 10:30:30 +0100

    more examples

---
+++ src/Topic_variable.kt
@@ -0,0 +1,11 @@
+package `topic_variable`
+
+// version 1.1.2
+
+fun main(args: Array<String>) {
+    3.let {
+        println(it)
+        println(it * it)
+        println(Math.sqrt(it.toDouble()))
+    }
+}
