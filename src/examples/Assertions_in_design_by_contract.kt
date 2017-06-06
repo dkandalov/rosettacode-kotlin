e6b745919bc80bb2b6773ec21eadd0d4353fa1c7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-06-06 23:32:18 +0100

    more examples

---
+++ src/examples/Assertions_in_design_by_contract.kt
@@ -0,0 +1,10 @@
+package `assertions_in_design_by_contract`
+
+// version 1.1.2
+// requires -ea JVM option
+
+fun main(args: Array<String>) {
+    assert(args.size > 0)  { "At least one command line argument must be passed to the program" }
+    println("The following command line arguments have been passed:")
+    for (arg in args) println(arg)
+}
