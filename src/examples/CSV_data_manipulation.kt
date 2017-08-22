532cab8ba315743bb7e4b3222c9476d70b191180
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-08-22 23:17:43 +0100

    more examples

---
+++ src/examples/CSV_data_manipulation.kt
@@ -0,0 +1,16 @@
+package `csv_data_manipulation`
+
+// version 1.1.3
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val lines = File("example.csv").readLines().toMutableList()
+    lines[0] += ",SUM"
+    for (i in 1 until lines.size) {
+        lines[i] += "," + lines[i].split(',').sumBy { it.toInt() }
+    }
+    val text = lines.joinToString("\n")
+    File("example2.csv").writeText(text)  // write to new file
+    println(text)  // print to console
+}
---
+++ src/examples/Test_a_function.kt
@@ -0,0 +1,17 @@
+package `test_a_function`
+
+// version 1.1.3
+
+fun isPalindrome(s: String) = (s == s.reversed())
+
+fun main(args: Array<String>) {
+    val testCases = listOf("racecar", "alice", "eertree", "david")
+    for (testCase in testCases) {
+        try {
+            assert(isPalindrome(testCase)) { "$testCase is not a palindrome" }
+        }
+        catch (ae: AssertionError) {
+            println(ae.message)
+        }
+    }
+}
