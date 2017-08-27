d74dc0aebe3303a20141b17379dc4d3aedc8642d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-08-27 21:29:29 +0100

    more examples

---
+++ src/examples/Echo_server.kt
@@ -0,0 +1,52 @@
+package `echo_server`
+
+// version 1.1.3
+
+import java.io.BufferedReader
+import java.io.PrintWriter
+import java.io.InputStreamReader
+import java.net.ServerSocket
+import java.net.Socket
+
+class ClientHandler(val clientSocket: Socket): Runnable {
+    private val connectionId: Int
+
+    init {
+        connectionId = ++numConnections
+        println("Handling connection, #$connectionId")
+    }
+
+    override fun run() {
+        val pw = PrintWriter(clientSocket.outputStream, true)
+        val br = BufferedReader(InputStreamReader(clientSocket.inputStream))
+        while (true) {
+            val line = br.readLine()
+            if (line == null) break
+            println("Received: $line")
+            pw.write("$line\n")
+            pw.flush()
+            if (line == "exit") break
+        }
+        br.close()
+        pw.close()
+        clientSocket.close()
+        println("Closing connection, #$connectionId")
+    }
+
+    private companion object {
+        var numConnections = 0
+    }
+}
+
+fun main(args: Array<String>) {
+    val serverSocket = ServerSocket(12321)
+    try {
+        while (true) {
+           Thread(ClientHandler(serverSocket.accept())).start()
+        }
+    }
+    finally {
+        serverSocket.close()
+        println("Closing server socket")
+    }
+}
