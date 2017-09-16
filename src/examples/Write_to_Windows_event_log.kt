ae5b70fbcb069c73f31d1ab5299947da7d4747a1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-09-16 17:29:21 +0100

    more examples

---
+++ src/examples/Write_to_Windows_event_log.kt
@@ -0,0 +1,14 @@
+package `write_to_windows_event_log`
+
+// version 1.1.4-3
+
+fun main(args: Array<String>) {
+    val command = "EventCreate" +
+                  " /t INFORMATION" +
+                  " /id 123" +
+                  " /l APPLICATION" +
+                  " /so Kotlin" +
+                  " /d \"Rosetta Code Example\""
+
+    Runtime.getRuntime().exec(command)
+}
