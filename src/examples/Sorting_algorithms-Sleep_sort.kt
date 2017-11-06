bc03348ab3152ae9d4c5a034737cf72489792ca7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-11-06 23:46:38 +0000

    more examples

---
+++ src/examples/Sorting_algorithms-Sleep_sort.kt
@@ -0,0 +1,25 @@
+package `sorting_algorithms_sleep_sort`
+
+// version 1.1.51
+
+import kotlin.concurrent.thread
+
+fun sleepSort(list: List<Int>, interval: Long) {
+    print("Sorted  : ")
+    for (i in list) {
+        thread {
+            Thread.sleep(i * interval)
+            print("$i ")
+        }
+    }
+    thread { // print a new line after displaying sorted list
+        Thread.sleep ((1 + list.max()!!) * interval)
+        println()
+    }
+}
+
+fun main(args: Array<String>) {
+   val list = args.map { it.toInt() }.filter { it >= 0 } // ignore negative integers
+   println("Unsorted: ${list.joinToString(" ")}")
+   sleepSort(list, 50)
+}
---
+++ src/examples/Synchronous_concurrency.kt
@@ -0,0 +1,32 @@
+package `synchronous_concurrency`
+
+// version 1.1.51
+
+import java.io.File
+import java.util.concurrent.SynchronousQueue
+import kotlin.concurrent.thread
+
+val queue = SynchronousQueue<String>()
+
+const val EOT = "\u0004"  // end of transmission
+
+fun main(args: Array<String>) {
+    thread {
+        var count = 0
+
+        while (true) {
+             val line = queue.take()
+             if (line == EOT) {
+                queue.put(count.toString())
+                break
+             }
+             println(line)
+             count++
+        }
+    }
+
+    File("input.txt").forEachLine { line -> queue.put(line) }
+    queue.put(EOT)
+    val count = queue.take().toInt()
+    println("\nNumber of lines printed = $count")
+}
