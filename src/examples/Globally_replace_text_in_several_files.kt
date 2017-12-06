2e03315c4661cd4d7b61c0625bdf0383948be2c5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-12-06 18:41:10 +0000

    §

---
+++ src/examples/Globally_replace_text_in_several_files.kt
@@ -0,0 +1,17 @@
+package `globally_replace_text_in_several_files`
+
+// version 1.2.0
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val files = arrayOf("file1.txt", "file2.txt")
+    for (file in files) {
+        val f = File(file)
+        var text = f.readText()
+        println(text)
+        text = text.replace("Goodbye London!", "Hello New York!")
+        f.writeText(text)
+        println(f.readText())
+    }
+}
