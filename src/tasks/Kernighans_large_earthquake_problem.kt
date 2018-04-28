41a4faea84f8c3491e069f94369076f9f2703a32
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-04-28 19:46:48 +0100

    more examples

---
+++ src/tasks/Kernighans_large_earthquake_problem.kt
@@ -0,0 +1,13 @@
+package `kernighans_large_earthquake_problem`
+
+// Version 1.2.40
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val r = Regex("""\s+""")
+    println("Those earthquakes with a magnitude > 6.0 are:\n")
+    File("data.txt").forEachLine {
+        if (it.split(r)[2].toDouble() > 6.0) println(it)
+    }
+}
