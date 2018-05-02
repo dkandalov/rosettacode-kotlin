51c136546069398ddd028bf9cd9eb1921c091bd5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-05-02 16:58:22 +0100

    more examples

---
+++ src/tasks/Reflection-List_methods.kt
@@ -0,0 +1,26 @@
+package `reflection_list_methods`
+
+// Version 1.2.31
+
+import kotlin.reflect.full.functions
+
+open class MySuperClass {
+    fun mySuperClassMethod(){}
+}
+
+open class MyClass : MySuperClass() {
+    fun myPublicMethod(){}
+
+    internal fun myInternalMethod(){}
+
+    protected fun myProtectedMethod(){}
+
+    private fun myPrivateMethod(){}
+}
+
+fun main(args: Array<String>) {
+    val c = MyClass::class
+    println("List of methods declared in ${c.simpleName} and its superclasses:\n")
+    val fs = c.functions
+    for (f in fs) println("${f.name}, ${f.visibility}")
+}
