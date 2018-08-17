1e1b5caa52b9b6bd0a162d3b5e867824b863d300
Dmitry Kandalov <dmitry.kandalov@springernature.com>
2018-08-17 14:10:31 +0100

    more examples

---
+++ src/tasks/Square_but_not_cube.kt
@@ -0,0 +1,20 @@
+package `square_but_not_cube`
+
+// Version 1.2.60
+
+fun main(args: Array<String>) {
+    var n = 1
+    var count = 0
+    while (count < 30) {
+        val sq = n * n
+        val cr = Math.cbrt(sq.toDouble()).toInt()
+        if (cr * cr * cr != sq) {
+            count++
+            println(sq)
+        }
+        else {
+            println("$sq is square and cube")
+        }
+        n++
+    }
+}
